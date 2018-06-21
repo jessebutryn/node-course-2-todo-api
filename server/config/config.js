0bdd5c8b81ab2dcc4a5f795d5bc84973aea730a2
Jesse Butryn <jesse.butryn@joyent.com>
2018-06-21 11:05:49 -0600

    Updated config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://jesse_b:test!234@ds127490.mlab.com:27490/todo-app'
-} else if (env === 'test') {
-	process.env.PORT = 3000;
-	process.env.MONGODB_URI = 'mongodb://jesse_b:test!234@ds229450.mlab.com:29450/todotest'
+if (env === 'development' || env === 'test') {
+	var config = require('./config.json');
+	var envConfig = config[env];
+
+	Object.keys(envConfig).forEach((key) => {
+		process.env[key] = envConfig[key];
+	});
 }
